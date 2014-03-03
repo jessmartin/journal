e863f9d9bda00af2478530582da26c48189d2971
Jess Martin <jessmartin@gmail.com>
2014-03-03 16:16:39 -0500

    Add prev and next navigation.

--- animations/js/main.js
+++ animations/js/main.js
@@ -1,1 +1,23 @@
+$(function() {
+  $(".next").click(function (e) {
+    var parent = $(this).parents(".page");
+    var newCurrentPage = $(parent).next();
 
+    if (newCurrentPage) {
+      parent.css('z-index', '102');
+      newCurrentPage.css('z-index', 103);
+      e.preventDefault();
+    }
+  });
+
+  $(".prev").click(function (e) {
+    var parent = $(this).parents(".page");
+    var newCurrentPage = $(parent).prev();
+
+    if (newCurrentPage) {
+      parent.css('z-index', '102');
+      newCurrentPage.css('z-index', 103);
+      e.preventDefault();
+    }
+  });
+});
